d3ca97a6dba065627546c40747551e1599dbfe97
unknown <ajibolamubarak304@gmail.com>
2022-01-03 06:49:38 +0100

    working on the JS file(3)

--- script.js
+++ script.js
@@ -16,3 +16,8 @@ const openModal = function (e) {
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
 };
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
