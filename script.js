04210d5f7a0a3f1a2e39a017502e0cb64938b1a3
unknown <ajibolamubarak304@gmail.com>
2022-01-04 23:10:04 +0100

    working on the JS file(7)

--- script.js
+++ script.js
@@ -51,3 +51,13 @@ btnScrollTo.addEventListener("click", function (e) {
 
   section1.scrollIntoView({ behavior: "smooth" });
 });
+/////////////////////////////////////////Page navigation
+
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
